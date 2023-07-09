88c2138c840b99c1849c87be6cb9c14bf7e4715b
ryo.nagata <ryo.nagata@chillnn.com>
2023-07-09 16:29:21 +0900

    initial commit

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // The code that defines your stack goes here
+        new s3.Bucket(this, 'MyFirstBucketCreatedByCdk', {
+            versioned: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // example resource
+        // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
+        //   visibilityTimeout: cdk.Duration.seconds(300)
+        // });
+    }
 }
